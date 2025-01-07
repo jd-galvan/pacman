7564b56f22356da96ac5b3ed6cdfa547151ba459
Daniel-A4F <jgalvan@agents4future.com>
2025-01-07 22:35:35 +0100

    Doxigenacion a levelmanager y scoreDisplay

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Controla las transiciones entre escenas.
+/// </summary>
 public class LevelManager : MonoBehaviour
 {
+  /// <summary>
+  /// Inicia el juego.
+  /// </summary>
   public void StartButton()
   {
     SceneManager.LoadScene(1);
   }
+
+  /// <summary>
+  /// Transiciona al menu de inicio
+  /// </summary>
   public void MenuButton()
   {
     SceneManager.LoadScene(0);
--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Representa la pantalla de score final.
+/// </summary>
 public class ScoreDisplay : MonoBehaviour
 {
-    [SerializeField] private Text scoreText;
+  [SerializeField] private Text scoreText;
 
-    private void Start()
-    {
-        scoreText.text = "Final Score: " + GameManager.finalScore.ToString();
-    }
+  /// <summary>
+  /// Muestra score final.
+  /// </summary>
+  private void Start()
+  {
+    scoreText.text = "Final Score: " + GameManager.finalScore.ToString();
+  }
 }
